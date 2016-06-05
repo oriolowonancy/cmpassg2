6d5cbf99092e47602c28a8f3a75bcad17d2071ae
Oriolowo Nancy <oriolowonancy@gmail.com>
2016-06-06 00:37:03 +0100

    nancee commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicing_the_divisibility_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            Console.Write("Welcome, please input the number to be tested: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num > 0)
+            {
+                if (num % 8 == 0) 
+                {
+                    Console.WriteLine("this value is divisible by 2,4 and 8");
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    Console.WriteLine("this value is not divisible by 2,4 and 8");
+                    Console.ReadLine(); 
+                }
+            }
+            else {
+                   Console.WriteLine("any number less than 0 is not premitted for testing");
+                   Console.ReadLine();
+            }
+        }
+    } 
+ }
+    
+
